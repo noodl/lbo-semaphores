c1b35d1e4725f3f7f0950ccd926ae71a99c05259
Vincent Bray <noodlet@gmail.com>
2023-11-19 15:05:54 +0000

    Simplify signalling

--- ch3/signalling.c
+++ ch3/signalling.c
@@ -10,17 +10,13 @@ from thread 1.
 
 sem_t semaphore;
 
-void* thread_function1(void* ptr) {
+void* thread_function1(void* arg) {
     sem_wait(&semaphore);
-    char *message;
-    message = (char *) ptr;
-    printf("%s \n", message);
+    printf("Thread 1\n");
 }
 
-void* thread_function2(void* ptr) {
-    char *message;
-    message = (char *) ptr;
-    printf("%s \n", message);
+void* thread_function2(void* arg) {
+    printf("Thread 2\n");
     sem_post(&semaphore);
 }
 
@@ -32,8 +28,8 @@ int main() {
     char *message1 = "Thread 1";
     char *message2 = "Thread 2";
 
-    pthread_create(&thread1, NULL, thread_function1, (void*) message1);
-    pthread_create(&thread2, NULL, thread_function2, (void*) message2);
+    pthread_create(&thread1, NULL, thread_function1, NULL);
+    pthread_create(&thread2, NULL, thread_function2, NULL);
 
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
